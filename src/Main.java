d25adc84dffeac6a9aa883943d6e1d234fb7c3fb
dctii <danactolman@gmail.com>
2023-10-26 12:33:25 -0700

    added general comments

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,25 @@ import utils.Sort;
 
 public class Main {
     public static void main(String[] args) {
+        /* Assignment 1 start */
         String[] sortTypes = {"ascending", "descending"};
 
+        // Loop for both sort types
         for (String sortType : sortTypes) {
             System.out.println(sortType + ":");
+            // Loop quick sorting of numbers arrays
             for (Integer[] numbers : Data.allNumbersArrays) {
                 Sort.quick(numbers, sortType);
                 System.out.println(Arrays.toString(numbers));
             }
 
+            // Loop quick sorting of letters arrays
             for (Character[] letters : Data.allLettersArrays) {
                 Sort.quick(letters, sortType);
                 System.out.println(Arrays.toString(letters));
             }
             System.out.println("\n");
         }
+        /* Assignment 1 end */
     }
 }
